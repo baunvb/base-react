98ab541c2486b88123e3100a85c5f1d2a893aee4
baunvb <baunvb@gmail.com>
2020-03-15 22:39:12 +0700

    update img url

--- src/config/host.js
+++ src/config/host.js
@@ -1,6 +1,6 @@
 const host = "https://serverrelease.whalelo.xyz/"
 // const host = "http://107.113.194.132:3002/" //dev
 
-const BASE_URL_IMG = "https://serverrelease.whalelo.xyz/image/"
+const BASE_URL_IMG = ""
 // const BASE_URL_IMG = "http://107.113.194.132:3002/image/"
 export {host, BASE_URL_IMG};
